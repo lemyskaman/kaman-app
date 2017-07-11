86abf24e7e5d1133782ac17a354157c625a0518b
lemys lopez <lemyskaman@gmail.com>
2017-07-11 00:21:14 -0400

    adding general modules access menu

--- kaman.app.js
+++ kaman.app.js
@@ -19,22 +19,27 @@ var Kapp = Mn.Application.extend({
     region: '#root',
 
 
-    radioRequests:{
-      'module:show':'moduleShow'
+    radioRequests: {
+        'module:show': 'moduleShow',
+        
     },
-    moduleShow:function(data){
-      console.log('module to show '+data.get('action'));
+    moduleShow: function (data) {
+        console.log('module to show ' + data.get('name'));
 
-      
-      data.set({status:true})
+        console.log(new _.findWhere(this.modules,{name:data.get('name')}).MainView())
+        //with underscore first we search for a module with the proper name and then
+        // we get its view constructor and build a view and pass a parameter 
+        //this.region.showChildView('page',new _.findWhere(this.modules,{name:data.get('name')}).MainView())
+
+        data.set({ status: true })
     },
     kamanInit: function () {
 
-        this.mergeOptions(this.options,_.keys(this.options));
+        this.mergeOptions(this.options, _.keys(this.options));
     },
     initialize: function () {
         this.kamanInit()
-        console.log('kamanapp insrance ',this)
+        console.log('kamanapp insrance ', this)
         if (config.get('debug'))
             console.log('kamanApp: ' + config.get('name')
                 + '\nwas initialized and will be rendered on '
@@ -47,16 +52,20 @@ var Kapp = Mn.Application.extend({
             name: 'KamanApp UI',
             langSource: this.langSource,
             appChannel: this.getChannel()//we pass the appchanel to use
+
         });
+
+
+        //building the menu
+        var menu = _.map(this.modules, function (v, k) {
+            return _.pick(v,'caption','name','icon')            
+        });
+        Backbone.Radio.channel('kaman:ui').request('menu').reset(menu)
 
         if (!_.isElement($(this.region)[0])) {
             console.warn('kamanApp: ' + config.get('name') + '\nit seams that ' + config.get('nodeSelector') + ' is not a DOM element')
         }
-
-
         //this.interface.set_mainView()
-
-
     },
 
     onStart: function () {
